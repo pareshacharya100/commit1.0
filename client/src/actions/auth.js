56a732387cf9e5b571a8ea3d2c3ba21b2ffa02fa
Paresh Acharya <pareshacharya@pareshs-mbp.mynetworksettings.com>
2024-08-25 20:10:41 -0400

    cors

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,21 +1,13 @@
-// src/actions/auth.js
+// actions/auth.js
+
 const api = require('../utils/api');
 const { setAlert } = require('./alert');
-const {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
-  USER_LOADED,
-  AUTH_ERROR,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT,
-} = require('./types');
+const { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } = require('./types');
 
 // Load User
 const loadUser = () => async (dispatch) => {
   try {
     const res = await api.get('/api/auth');
-
     dispatch({
       type: USER_LOADED,
       payload: res.data,
@@ -31,7 +23,6 @@ const loadUser = () => async (dispatch) => {
 const register = (formData) => async (dispatch) => {
   try {
     const res = await api.post('/api/users', formData);
-
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data,
@@ -39,18 +30,39 @@ const register = (formData) => async (dispatch) => {
     dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
-
     if (errors) {
       errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
-
     dispatch({
       type: REGISTER_FAIL,
     });
   }
 };
 
-// Exporting functions
+// Login User
+const login = (email, password) => async (dispatch) => {
+  const body = { email, password };
+  try {
+    const res = await api.post('/api/auth', body);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
+
+// Logout
+const logout = () => ({ type: LOGOUT });
+
 module.exports = {
   loadUser,
   register,
